8bc356d03e4d5fa27a1af87c3890964f3fc2bcc5
Yuqing Hu <yuqing.hu@sdusz.com.cn>
2016-04-29 16:38:59 +0800

    登录

---
+++ web/src/ccc/login/js/main/login.js
@@ -0,0 +1,121 @@
+'use strict';
+var CommonService = require('ccc/global/js/modules/common').CommonService;
+var captChaImg = $('.captcha-img');
+var captcha = {};
+
+
+getCaptCha();
+
+$('.change-captcha').on('click', function (e) {
+    e.preventDefault();
+    getCaptCha();
+});
+
+//$("[name = checkbox]").attr("checked", true);
+
+function getCaptCha() {
+    CommonService.getCaptcha(function (data) {
+        captcha = data;
+        captChaImg.attr('src', data.captcha);
+    });
+}
+
+//var loginRac = new Ractive({
+//    el: $('.s-login-view'),
+//    template: require('ccc/login/partials/login.html'),
+//    data: {
+//        captcha:{
+//            img: '',
+//            token: ''
+//        }
+//    }
+//});
+//CommonService.getCaptcha(function (res) {
+//    loginRac.set('captcha', {
+//        img: res.captcha,
+//        token: res.token
+//    });
+//});
+//loginRac.on('change-captcha',function(){
+//    CommonService.getCaptcha(function (res) {
+//        loginRac.set('captcha', {
+//            img: res.captcha,
+//            token: res.token
+//        });
+//    });
+//});
+
+var errorRac = new Ractive({
+    el: $('.error-wrap'),
+    template: require('ccc/login/partials/error.html'),
+    data: {
+        error: null
+    }
+});
+
+$('#loginForm').submit(function (e) {
+    var $this = $(this);
+    e.preventDefault();
+    var $loginName = $('input[name=loginName]');
+    var $password = $('input[name=password]');
+    var $postBtn = $('#login_button');
+    var $errorMobile = $('.mobile');
+    var $errorPwd = $('.password');
+    var $error = $('.loginlock');
+
+    $errorMobile.empty();
+    $errorPwd.empty();
+    $error.empty();
+
+    if ($loginName.val() === '') {
+        $errorMobile.text('手机号不能为空');
+        return;
+    }
+    if ($password.val() === '') {
+        $errorPwd.text('密码不能为空');
+        return;
+    }
+
+    var errorMaps = {
+        USER_DISABLED: '帐号密码错误次数过多，您的帐户已被锁定，请联系客服4001-718-718解锁。',
+        FAILED: '你输入的账户名或密码错误。请重新输入！您也可以找回登录密码。',
+        TOO_MANY_ATTEMPT: '密码输入次数过多，该用户已被禁用'
+    };
+
+    if ($postBtn.hasClass('disabled')) {
+        return;
+    }
+
+    $postBtn.addClass('disabled').html('登录中...');
+
+    request.post('/api/web/login').type('form').send($this.serialize()).end().get('body').then(function (r) {
+        if (r.success) {
+            $postBtn.text('登录成功');
+            var url = /(loan)/;
+            if (url.test(document.referrer)) {
+                location.href = document.referrer;
+                return;
+            }
+            if (CC.user.enterprise) {
+                location.pathname = "/newAccount/home";
+            } else {
+                location.href = (r.redirect) ? r.redirect : '/';
+            }
+        } else {
+            $error.html('<p class="login-error">'+errorMaps[r.error_description.result]+'</p>');
+            $postBtn.removeClass('disabled').text('登录');
+        }
+    });
+
+    return false;
+});
+
+request.get(encodeURI('/api/v2/cms/category/IMAGE/name/登录')).end().then(function (res) {
+    var count = new Ractive({
+        el: '.loginBanner',
+        template: '{{#each items}}<a href="{{url}}" target="_blank"><img src="{{content}}"/></a>{{/each}}',
+        data: {
+            items: res.body
+        }
+    });
+});
--- web/src/ccc/login/routers/page.js
+++ web/src/ccc/login/routers/page.js
@@ -1,14 +1,65 @@
 'use strict';
-var middlewares = require('../middlewares');
-var bodyParser = require('cc-body');
-
+var _ = require('lodash');
+var url = require('url');
+var crypto = require('crypto');
+var config = require('config');
+var conext = require('conext');
+var proagent = require('promisingagent');
+var crypto = require('crypto');
+//var xml2json = require('xml2json');
+// var wxrequest = require('./wxrequest');
+var db = require('@cc/redis');
+var log = require('bunyan-hub-logger')({ app: 'web', name: 'wx' })
 module.exports = function (router) {
-    router.get('/', middlewares.setBackUrl, middlewares.loginPage);
+    router.get('/', function (req, res) {
+        _.assign(res.locals, {
+//            title : '718_金融理财平台',
+//            keywords : '718_金融理财平台',
+//            description : '718_金融理财平台'
+        });
+        res.render();
+    });
+    router.get('/quickLogin/:mobile/:currentTime/:md5key', function *(req, res) {
 
-    router.post('/',
-        bodyParser,
-        middlewares.setBackUrl,
-        middlewares.checkCaptcha,
-        middlewares.doLogin);
+        var r = yield req.uest('/api/v2/quickLogin/'+req.params.mobile+'/'+req.params.currentTime+'/'+req.params.md5key);
+        if(!r.body.success){
+            res.redirect('/');
+            return;
+        }
 
-};
+        if(!r.body.data.isNewUser){
+            var signInUser = Promise.coroutine(function *(user) {
+                var obj = {
+                    user: user,
+                    client: {
+                        name: 'node',
+                        id: config.oauth2client.id,
+                    },
+                    scope: [],
+                };
+                var ccat = yield randomHex(32);
+                yield db.setex('access_token:' + ccat, 24 * 60 * 60, JSON.stringify(obj));
+                return ccat;
+            });
+            var ccat = yield signInUser(r.body.data.user);
+            res.cookie('ccat', ccat, {
+                maxAge: 24 * 60 * 60
+            });
+            res.redirect('/');
+        }
+        if(r.body.data.isNewUser) {
+            res.redirect('/newAccount/setpassword?mobile=' + req.params.mobile + '&currentTime=' + req.params.currentTime + '&md5key=' + req.params.md5key);
+        }
+
+    })
+}
+function randomHex(len) {
+    return new Promise(function (resolve, reject) {
+        crypto.randomBytes(len, function (err, buf) {
+            if (err) {
+                return reject(err);
+            }
+            resolve(buf.toString('hex'));
+        });
+    });
+}
