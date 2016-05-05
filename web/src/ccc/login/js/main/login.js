2315a3b4ffc8dcb551f381518ae47db43bff17be
zfc <1092674723@qq.com>
2016-05-05 14:22:43 +0800

    add

--- web/src/ccc/login/js/main/login.js
+++ web/src/ccc/login/js/main/login.js
@@ -90,6 +90,7 @@ $('#loginForm').submit(function (e) {
 
     request.post('/api/web/login').type('form').send($this.serialize()).end().get('body').then(function (r) {
         if (r.success) {
+            console.log('--', r)
             $postBtn.text('登录成功');
             var url = /(loan)/;
             if (url.test(document.referrer)) {
