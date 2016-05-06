fa2d3d00a6bada6cec5e2cbd80f74b9850e8186b
Qiaoyun <qiaoyun.yu@sdusz.com.cn>
2016-05-06 17:40:59 +0800

    touziye

--- web/src/ccc/invest/routers/page.js
+++ web/src/ccc/invest/routers/page.js
@@ -12,14 +12,18 @@ module.exports = function (router) {
             '718金融理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
 
         //var productKey = ['XNB', 'FB', 'XJB'];
-		var productKey = ['XNB', 'HDZX','XSZX'];
+		var productKey = ['SBTZ', 'HOT','LHB','DCB','NEW'];
         res.locals.products = [];
-        var productKeyXSZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
-        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
-        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
-        res.locals.products.push(productKeyXSZX);
-		res.locals.products.push(productKeyHDZX);
-		res.locals.products.push(productKeyLCZQ);
+        var productKeySBTZ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
+        var productKeyHOT=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
+        var productKeyLHB=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        var productKeyDCB=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        var productKeyNEW=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        res.locals.products.push(productKeySBTZ);
+        res.locals.products.push(productKeyHOT);
+        res.locals.products.push(productKeyLHB);
+        res.locals.products.push(productKeyDCB);
+        res.locals.products.push(productKeyNEW);
         res.expose('', 'product');
         res.render();
         return false;
@@ -36,14 +40,18 @@ router.get('/:product', async function (req, res) {
             '718bank理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
 
         //var productKey = ['XNB', 'FB', 'XJB'];
-		var productKey = ['XNB', 'HDZX','XSZX'];
+		var productKey = ['SBTZ', 'HOT','LHB','DCB','NEW'];
         res.locals.products = [];
-        var productKeyXSZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
-        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
-        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
-        res.locals.products.push(productKeyXSZX);
-        res.locals.products.push(productKeyHDZX);
-        res.locals.products.push(productKeyLCZQ);
+        var productKeySBTZ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
+        var productKeyHOT=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
+        var productKeyLHB=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        var productKeyDCB=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        var productKeyNEW=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        res.locals.products.push(productKeySBTZ);
+        res.locals.products.push(productKeyHOT);
+        res.locals.products.push(productKeyLHB);
+        res.locals.products.push(productKeyDCB);
+        res.locals.products.push(productKeyNEW);
         res.expose(req.params.product, 'product')
         res.render('index');
         return false;
