76baaea9c5f571f30fe66735a515f6bd102f710d
Yuqing Hu <yuqing.hu@sdusz.com.cn>
2016-05-25 15:47:53 +0800

    关于我们初步修改

--- web/src/ccc/help/js/main/common.js
+++ web/src/ccc/help/js/main/common.js
@@ -1,10 +1,6 @@
 'use strict';
 
-//var $ = require('jquery');
 require('bootstrap/js/tab');
-
-
-
 
 
 var $preArrow = null;
@@ -17,10 +13,6 @@ $('.ar-title-wp').on('click', function () {
     var right = 'show';
     var down = 'closed';
 
-
-//     $('.article-wp').removeClass(klass);
-    
-//     $wp.addClass(klass);
      if($arrow.hasClass(right)){
             $content.css('display','block'); 
             $arrow.removeClass(right).addClass(down);
@@ -33,17 +25,5 @@ $('.ar-title-wp').on('click', function () {
 
 var hash = location.hash;
 
-// 自动展开三方账户
-//if (hash === '#paymentAccount') {
-//    var $wp = $('.invest-wrapper h2');
-//    $wp.eq($wp.length - 1).addClass('opened');
-//}
-//
-//$(".invest-wrapper p").hide()
-//$(".invest-wrapper p").ready(function(){
-//  $(".invest-wrapper h2").click(function(){
-//  $(".opened .invest-wrapper p").toggle();
-//  });
-//});
 
 
