9bc1caf3c6101a1e0cc8de2a14d5ee63e25af86b
Yuqing Hu <yuqing.hu@sdusz.com.cn>
2016-06-02 17:34:30 +0800

    fix 278

--- web/src/ccc/newAccount/js/main/settings/bankCards.js
+++ web/src/ccc/newAccount/js/main/settings/bankCards.js
@@ -1,15 +1,10 @@
 "use strict";
 
 var utils = require('ccc/global/js/lib/utils');
-// var LLPBANKS = require('ccc/global/js/modules/cccUmpBanks');
 var Confirm = require('ccc/global/js/modules/cccConfirm');
 var accountService = require('ccc/newAccount/js/main/service/account').accountService;
 var CommonService = require('ccc/global/js/modules/common').CommonService;
 var CccOk = require('ccc/global/js/modules/cccOk');
-// 过滤银行卡，只显示enabled=true的
-// var banks = _.filter(LLPBANKS, function (r) {
-//     return r.enable === true;
-// });
 
 if (CC.user.account) {
     CC.user.account.Faccount = utils.bankAccount(CC.user.account.account);
@@ -40,8 +35,6 @@ var ractive = new Ractive({
         ifDel: false,
         mobile: CC.user.mobile,
         realName: CC.user.name,
-        isAuditing : CC.user.fundaccountsMap.data.auditingList.length > 0 ? true : false,
-        authenticated: CC.user.authenticates.idauthenticated || false,
         paymentPasswordHasSet: CC.user.paymentPasswordHasSet
     },
     oninit: function () {
@@ -63,6 +56,7 @@ var ractive = new Ractive({
     }
 });
 
+//校验表单
 ractive.on("validateCardNo", function () {
     var no = this.get("cardNo");
     if (!/^\d*$/.test(no)||no=='') {
@@ -79,17 +73,6 @@ ractive.on("validateIdNo", function () {
         this.set("idNoError", false);
     }
 });
-
-
-ractive.on('checkSmsCaptcha', function () {
-    var smsCaptcha = this.get('smsCaptcha');
-    if (smsCaptcha === '') {
-        this.set('SMS_NULL', true);
-    } else {
-        this.set('SMS_NULL', false);
-    }
-});
-
 ractive.on("validatePhoneNo", function () {
     var no = this.get("phoneNo");
     if (!/^\d*$/.test(no)) {
@@ -98,6 +81,14 @@ ractive.on("validatePhoneNo", function () {
         this.set("phoneNoError", false);
     }
 });
+ractive.on("validateCaptcha", function () {
+    var no = this.get("smsCaptcha");
+    if (no === '') {
+        this.set('SMS_NULL', true);
+    } else {
+        this.set('SMS_NULL', false);
+    };
+});
 
 ractive.on('doDel', function () {
     this.set('ifDel',true);
@@ -105,21 +96,51 @@ ractive.on('doDel', function () {
 
 ractive.on("bind-card-submit", function (e) {
     e.original.preventDefault();
-    var cardNoError = this.get("cardNoError");
-    var idNoError = this.get("idNoError");
-    var phoneNoError = this.get("phoneNoError");
-    var SMS_NULL = this.get('SMS_NULL');
-    if (cardNoError || phoneNoError || idNoError||SMS_NULL) {
-        return false;
-    }
+//    var cardNoError = this.get("cardNoError");
+//    var idNoError = this.get("idNoError");
+//    var phoneNoError = this.get("phoneNoError");
+//    var SMS_NULL = this.get('SMS_NULL');
+//    if (cardNoError || phoneNoError || idNoError||SMS_NULL) {
+//        return false;
+//    }
     var bankName = this.get('bankName');
     var cardNo = this.get('cardNo');
     var idNo = this.get('idNo');
     var personal=this.get('personal');
     var cardPhone = this.get('mobile');
     var smsCaptcha = this.get('smsCaptcha');
-
-
+    //校验表单
+    if (!/^\d*$/.test(cardNo)||cardNo=='') {
+        this.set("cardNoError", true);
+    } else {
+        this.set("cardNoError", false);
+    };
+    
+    if (!/^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/.test(idNo)) {
+        this.set("idNoError", true);
+    } else {
+        this.set("idNoError", false);
+    };
+    
+    if(personal == ''){
+        this.set("personalError", true);
+    }else{
+        this.set("personalError", false);
+    };
+    
+    if (smsCaptcha === '') {
+        this.set('SMS_NULL', true);
+        return;
+    } else {
+        this.set('SMS_NULL', false);
+    };
+    
+    if (!/^\d*$/.test(cardPhone)) {
+        this.set("phoneNoError", true);
+    } else {
+        this.set("phoneNoError", false);
+    }
+    
     var sendObj = {
         bankCode: bankName,
         cardNo: cardNo,
@@ -144,24 +165,25 @@ ractive.on("bind-card-submit", function (e) {
       }else{
         $.post('/api/v2/hundsun/checkCard/MYSELF',sendCard,function(r){
           if(r.success){
+            ractive.set("cardNoError", false);
             $.post('/api/v2/hundsun/bindCard/MYSELF', sendObj, function (r) {
                 if(r.success) {
                   window.location.reload();
-                  // $('.bind-card-wrapper').css('display','none');
-                  // $('.binded-card-wrapper').css('display','block');
-                    // CccOk.create({
-                    //     msg: '绑卡成功',
-                    //     okText: '确定',
-                    //     ok: function () {
-                    //         window.location.reload();
-                    //     },
-                    //     cancel: function () {
-                    //         window.location.reload();
-                    //     }
-                    // });
                 } else {
+                    var msg = {
+                        SEND_CAPTCHA_FAILED: '验证码发送失败',
+                        SMSCAPTCHA_IS_NOT_CORRECT: '手机验证码不匹配',
+                        IDNUMBER_ALREADY_EXISTED: '此身份证号已被注册',
+                        REGISTER_FAILED: '开户失败',
+                        CHECK_CARD_FAILED: '验卡失败',
+                        BIND_CARD_FAILED: '绑卡失败',
+                        UNKNOWN: '系统繁忙，请稍后重试！',
+                        ACCESS_DENIED: '登录超时',
+                        
+                        SUCCEED: '银行卡绑定成功',
+                    };
                     CccOk.create({
-                        msg: '绑卡失败，' + r.error[0].message,
+                        msg: '绑卡失败，' + msg[r.error[0].message],
                         okText: '确定',
                         ok: function () {
                             window.location.reload();
@@ -172,6 +194,10 @@ ractive.on("bind-card-submit", function (e) {
                     });
                 }
             });
+          }else{
+              ractive.set("cardNoError", true);
+              ractive.set("errMessgaeBank", r.error[0].value);
+              return;
           }
         })
       }
@@ -234,13 +260,3 @@ function countDown() {
     }), 1000);
 }
 
-function clearErrorIndex(key,msgkey){
-    ractive.set(key,false);
-    ractive.set(msgkey,'');
-    return false;
-}
-function showErrorIndex(key,msgkey,msg){
-    ractive.set(key,true);
-    ractive.set(msgkey,msg);
-    return false;
-}
