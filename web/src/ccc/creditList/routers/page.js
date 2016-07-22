f531e208e35b8267e81a415fd13d20aee1a99ff7
Yuqing Hu <yuqing.hu@sdusz.com.cn>
2016-07-22 16:59:18 +0800

    fix

--- web/src/ccc/creditList/routers/page.js
+++ web/src/ccc/creditList/routers/page.js
@@ -12,6 +12,23 @@ module.exports = function (router) {
         res.locals.description =
             '理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
 
+        //转让总金额和总笔数
+        req.uest('/api/v2/creditassign/stat/total').end().then(function(r){
+            res.locals.total = r.body.data;
+            var total = res.locals.total;
+            res.expose(total, 'total');
+        });
+
+        //转让成交记录
+        req.uest('/api/v2/creditassign/stat/list').end().then(function(r){
+            console.log('====r.body')
+            console.log(r.body)
+            //res.locals.total = r.body.data;
+            //var total = res.locals.total;
+            //res.expose(total, 'total');
+        });
+
+
 //        var productKey = ['XSZX', 'HDZX', 'LCZQ'];
 //        res.locals.products = [];
 //        req.uest('/api/v2/loan/getLoanProduct/productKey/' +
