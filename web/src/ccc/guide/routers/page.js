65bcec3f40ac0aee20eafcd0b2d4c4dc60d080d6
sdusz <sdusz@sduszdeMac-mini.local>
2016-12-16 14:39:28 +0800

    修改

--- web/src/ccc/guide/routers/page.js
+++ web/src/ccc/guide/routers/page.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (router) {
-    router.get('/', async function (req, res) {
+    router.get('/', function (req, res) {
         var user = res.locals.user;
         res.expose(user, 'user');
 
@@ -12,14 +12,14 @@ module.exports = function (router) {
 
         res.expose(user, 'user');
 
-        res.locals.carousel = await req.uest('/api/v2/cms/category/HOMEPAGE/name/carousel')
+        res.locals.carousel = req.uest('/api/v2/cms/category/HOMEPAGE/name/carousel')
             .end()
             .get('body')
             .then(function (data) {
                 return data;
             });
 
-        res.locals.picture = await req.uest(encodeURI('/api/v2/cms/category/IMAGE/name/首页广告栏'))
+        res.locals.picture = req.uest(encodeURI('/api/v2/cms/category/IMAGE/name/首页广告栏'))
             .end()
             .get('body')
             .then(function (data) {
